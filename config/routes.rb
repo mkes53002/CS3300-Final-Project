30e0e4c04110f0e1479329e9a981accef44f5084
Katie <mkes53002@gmail.com>
2023-04-27 20:58:06 +0000

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
   #resources :pet_listings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  root "pet_listings#index"
+  root "home#index"
   resources :pet_listings
 end
