bef1a910a2f9b5e0ec52c83e66e6464135e39813
Katie <mkes53002@gmail.com>
2023-04-28 07:48:02 +0000

    adding more test coverage

--- spec/controller/pet_listings_functional_spec.rb
+++ spec/controller/pet_listings_functional_spec.rb
@@ -37,5 +37,59 @@ RSpec.describe PetListingsController, :type => :controller do
                 expect(response).to have_http_status(:ok)
             end
         end
+
+        # Tests that the controller completes GET HTTP request for #new
+        context "GET #new" do
+            it "returns a success response" do
+                get new_pet_listing_path
+                expect(response).to have_http_status(:ok)
+            end
+        end
+
+    describe "test POST #create" do
+        # Tests that the controller completes POST HTTP request
+        context "with valid parameters" do
+            it "creates a new Pet Listing" do
+                expect {
+                    post pet_listings_path, params: { pet_name: "Test Pet Name", owner_contact: "Test Owner Contact") }
+            }.to change (PetListing, :count).by(1)
+            end
+
+            it "redirects to the created listing" do
+                post pet_listings_path, params: { pet_name: "Test Pet Name", owner_contact: "Test Owner Contact") }
+                expect(response).to redirect_to(pet_listings_path(PetListing.last))
+            end
+
+            context "with invalid parameters" do
+                it "does not create a new listing" do
+                    expect {
+                        post pet_listings_path, params: { pet_name: "Test Pet Name", owner_contact: "Test Owner Contact") }
+                }.to change(PetListing, :count).by(0)
+                end
+                
+                it "renders a success response" do
+                    post pet_listings_path, params: { (pet_name: "Test Pet Name", owner_contact: "Test Owner Contact") }
+                    expect(response).to be_successful
+                end
+            end
+        end
+
+        describe "PATCH #update" do
+            context "with valid parameters" do
+                let(:new_attributes) {
+                    skip(pet_name: "Test Pet Name", owner_contact: "Test Owner Contact")
+                }
+            
+                it "updates the requested article" do
+                    pet_listing = PetListing.create(pet_name: "Test Pet Name", owner_contact: "Test Owner Contact")
+                    patch pet_listing(pet_listing), params: { pet_listing: new_attributes }
+                    pet_listing.reload
+                    expect(response).to redirect_to(pet_listing(pet_listing))
+                end
+            end
+
+            context "with invalid parameters" do
+                it "renders a successful response" do
+                    pet_listing = PetListing.create
     end
 end
--- spec/features/pet_listings_feature_spec.rb
+++ spec/features/pet_listings_feature_spec.rb
@@ -5,49 +5,50 @@ RSpec.feature "Pet Listing", type: :feature do
     context "Update Pet Listing" do
 
         # Sets up a pet listing to test edits on
-        let(:pet_listing) { PetListing.create(pet_name: "Test Pet Name", owner_contact: "Test Owner Contact") }
+        let!(:pet_listing) { PetListing.create(pet_name: "Test Pet Name", owner_contact: "Test Owner Contact") }
         
         # Navigates to the edit pet listing page
         before(:each) do
             user = FactoryBot.create(:user)
             login_as(user)
+            visit pet_listings_path
             visit edit_pet_listing_path(pet_listing)
         end
 
         # Makes sure when a user edits a pet name correctly they get the success page
         scenario "should be successful" do
             within("form") do
-                fill_in "Pet Name", with: "New Pet Name"
+                fill_in "Pet name", with: "New Pet Name"
             end
             click_button "Update Pet Listing"
-            expect(page).to have_content("Pet Listing was successfully updated")
+            expect(page).to have_content("Pet listing was successfully updated")
         end
  
         # Makes sure when a user forgets a pet name they get an error message
         scenario "should fail" do
             within("form") do
-                fill_in "Pet Name", with: ""
+                fill_in "Pet name", with: ""
             end
             click_button "Update Pet Listing"
-            expect(page).to have_content("Pet Name can't be blank")
+            expect(page).to have_content("Pet name can't be blank")
         end
 
         # Makes sure when a user edits a owner contact correctly they get the success page
         scenario "should be successful" do
             within("form") do
-                fill_in "Owner Contact", with: "New Owner Contact"
+                fill_in "Owner contact", with: "New Owner Contact"
             end
             click_button "Update Pet Listing"
-            expect(page).to have_content("Pet Listing was successfully updated")
+            expect(page).to have_content("Pet listing was successfully updated")
         end
  
         # Makes sure when a user forgets a owner contact they get an error message
         scenario "should fail" do
             within("form") do
-                fill_in "Owner Contact", with: ""
+                fill_in "Owner contact", with: ""
             end
             click_button "Update Pet Listing"
-            expect(page).to have_content("Owner Contact can't be blank")
+            expect(page).to have_content("Owner contact can't be blank")
         end
     end
         
@@ -62,28 +63,30 @@ RSpec.feature "Pet Listing", type: :feature do
         
         # Makes sure when a user forgets a pet name they get an error message
         scenario "should fail" do
-            within("form") do
-                fill_in "Pet Name", with: ""
-            end
+            #within("form") do
+            fill_in "Pet name", with: ""
+            fill_in "Owner contact", with: "Test contact"
+            fill_in "Last seen at", with: "Test location"
+            #end
             click_button "Create Pet Listing"
-            expect(page).to have_content("Pet Name can't be blank")
+            expect(page).to have_content("Pet name can't be blank")
         end
         
         # Makes sure when a user forgets a owner contact they get an error message
         scenario "should fail" do
             within("form") do
-                fill_in "Owner Contact", with: ""
+                fill_in "Owner contact", with: ""
             end
             click_button "Create Pet Listing"
-            expect(page).to have_content("Owner Contact can't be blank")
+            expect(page).to have_content("Owner contact can't be blank")
         end
         
         # Makes sure when a user creates a project correctly they get the success page
         scenario "should be successful" do
-            within("form") do
-                fill_in "Pet Name", with: "Test Pet Name"
-                fill_in "Owner Contact", with: "Test Owner Contact"
-            end
+            #within("form") do
+                fill_in "Pet name", with: "Test Pet Name"
+                fill_in "Owner contact", with: "Test Owner Contact"
+            #end
             click_button "Create Pet Listing"
             expect(page).to have_content("Pet Listing was successfully created")
         end 
